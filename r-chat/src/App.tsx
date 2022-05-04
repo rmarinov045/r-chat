1ab7a57ea55c3baddb6f6e17ec8b86f1f62ec1da
Radostin Marinov <r.marinov045@gmail.com>
2022-05-04 16:50:10 +0300

    Adding private routes Adding public routes Adding general-use spinner Re-configuring router

--- r-chat/src/App.tsx
+++ r-chat/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
 import Login from './components/login/Login';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 import ConfirmEmail from './components/register/ConfirmEmail';
 import Register from './components/register/Register';
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />}></Route>
+        <Route path='/' element={<PublicRoute component={<Login />} />}></Route>
         <Route path='/register'>
-          <Route index={true} element={<Register />}></Route>
-          <Route path='confirm-email' element={<ConfirmEmail />}></Route>
+          <Route index={true} element={<PublicRoute component={<Register />} />}></Route>
+          <Route path='confirm-email' element={<PublicRoute component={<ConfirmEmail />} />}></Route>
         </Route>
+        <Route path='/home' element={<PrivateRoute component={<Home />} />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ r-chat/src/components/PrivateRoute.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { auth } from '../firebase'
+import Spinner from './utils/Spinner'
+
+function PrivateRoute({ component } :{component: ReactElement}) {
+    const navigate = useNavigate()
+
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (!user) {
+                navigate('/')
+                return
+            }
+            setLoaded(true)
+        })
+        
+        return () => setLoaded(false)
+    }, [navigate])
+
+    return (
+        loaded ? component : <Spinner />
+    )
+}
+
+export default PrivateRoute
